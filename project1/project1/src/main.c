85d918e9ac58c28f15b3661c3de3fe302429b5ac
Sam Balana <sambalana247@gmail.com>
2018-04-12 16:45:46 -0700

    feat: Finish project 1

--- project1/project1/src/main.c
+++ project1/project1/src/main.c
@@ -3,28 +3,136 @@
 
 #define CLOCK_MHZ 1
 
-void wait(unsigned long ms)
+struct state;
+typedef void state_fn(struct state*);
+
+struct state 
 {
+	void (*next)(struct state*);
+	unsigned char button;
+	unsigned char counter;
+	unsigned char led_on;
+};
+
+state_fn start, down_on, down_off, up_on, up_off, wind_down;
+
+void start(struct state *state)
+{
+	if (state->button)
+	{
+		state->next = down_on;
+		state->led_on = 1;
+	}
+}
+
+void down_on(struct state *state)
+{
+	state->counter++;
+		
+	if (!state->button)
+		state->next = up_on;
+	
+	if (state->counter > 10)
+	{
+		state->next = down_off;
+		state->counter = 0;
+		state->led_on = 0;
+	}
+}
+
+void down_off(struct state *state)
+{
+	state->counter++;
+
+	if (!state->button)
+		state->next = up_off;
+	
+	if (state->counter > 10)
+	{
+		state->next = down_on;
+		state->counter = 0;
+		state->led_on = 1;
+	}
+}
+
+void up_on(struct state *state)
+{
+	state->counter++;
+	
+	if (state->button)
+	{
+		state->next = wind_down;
+		state->counter = 0;
+		state->led_on = 0;
+	}
+		
+	if (state->counter > 10)
+	{
+		state->next = up_off;
+		state->counter = 0;
+		state->led_on = 0;
+	}
+}
+
+void up_off(struct state *state)
+{
+	state->counter++;
+	
+	if (state->button)
+	{
+		state->next = wind_down;
+		state->counter = 0;
+	}
+	
+	if (state->counter > 10)
+	{
+		state->next = up_on;
+		state->counter = 0;
+		state->led_on = 1;
+	}
+}
+
+void wind_down(struct state *state)
+{	
+	if (!state->button)
+		state->next = start;
+}
+
+void wait(unsigned short ms)
+{
+	/*
 	volatile uint32_t counter = 0;
 	uint32_t loops = CLOCK_MHZ * 100 * ms / 3;
 	while (counter < loops)
 		counter++;
+	*/
+	wait_avr(ms);
 }
 
-int main (void)
+int main()
 {
+	// Initialize
 	board_init();
+	ini_avr();
 	
 	// Configure GPIO pins
-	DDRB |= (1 << 0);
+	DDRB |= (1 << 0);	// B0 is an output
+	DDRB &= ~(1 << 1);	// B1 is an input
 	
 	// Blink that damn LED
-	for (;;)
+	struct state s = { start, 0, 0, 0 };
+	while (s.next)
 	{
-		PORTB |= (1 << 0);
-		wait(500);
-		PORTB &= ~(1 << 0);
-		wait(500);
+		s.button = PINB & (1 << 1);
+		s.next(&s);
+		
+		if (s.led_on)
+			SET_BIT(PORTB, 0);
+		else
+			CLR_BIT(PORTB, 0);
+			
+		wait_avr(5);
 	}
 
+	return 0;
 }
