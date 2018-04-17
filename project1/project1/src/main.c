2c7143be580f69b7cb0cb461f12415891543be94
Sam Balana <sambalana247@gmail.com>
2018-04-17 10:51:43 -0700

    refactor: Add editorconfig formatting

--- project1/project1/src/main.c
+++ project1/project1/src/main.c
@@ -4,100 +4,100 @@
 #define CLOCK_MHZ 1
 
 struct state;
-typedef void state_fn(struct state*);
+typedef void state_fn(struct state *);
 
-struct state 
+struct state
 {
-	void (*next)(struct state*);
-	unsigned char button;
-	unsigned char counter;
-	unsigned char led_on;
+  void (*next)(struct state *);
+  unsigned char button;
+  unsigned char counter;
+  unsigned char led_on;
 };
 
 state_fn start, down_on, down_off;
 
 void start(struct state *state)
 {
-	if (state->button)
-	{
-		state->next = down_on;
-		state->led_on = 1;
-	}
+  if (state->button)
+  {
+    state->next = down_on;
+    state->led_on = 1;
+  }
 }
 
 void down_on(struct state *state)
 {
-	state->counter++;
-		
-	if (!state->button)
-	{
-		state->next = start;
-		state->counter = 0;
-		state->led_on = 0;
-	}
-	
-	if (state->counter > 10)
-	{
-		state->next = down_off;
-		state->counter = 0;
-		state->led_on = 0;
-	}
+  state->counter++;
+
+  if (!state->button)
+  {
+    state->next = start;
+    state->counter = 0;
+    state->led_on = 0;
+  }
+
+  if (state->counter > 10)
+  {
+    state->next = down_off;
+    state->counter = 0;
+    state->led_on = 0;
+  }
 }
 
 void down_off(struct state *state)
 {
-	state->counter++;
+  state->counter++;
 
-	if (!state->button)
-	{
-		state->next = start;
-		state->counter = 0;
-		state->led_on = 0;
-	}
-	
-	if (state->counter > 10)
-	{
-		state->next = down_on;
-		state->counter = 0;
-		state->led_on = 1;
-	}
+  if (!state->button)
+  {
+    state->next = start;
+    state->counter = 0;
+    state->led_on = 0;
+  }
+
+  if (state->counter > 10)
+  {
+    state->next = down_on;
+    state->counter = 0;
+    state->led_on = 1;
+  }
 }
 
 void wait(unsigned short ms)
 {
-	/*
+  /*
 	volatile uint32_t counter = 0;
 	uint32_t loops = CLOCK_MHZ * 100 * ms / 3;
 	while (counter < loops)
 		counter++;
 	*/
-	wait_avr(ms);
+  wait_avr(ms);
 }
 
 int main()
 {
-	// Initialize
-	board_init();
-	ini_avr();
-	
-	// Configure GPIO pins
-	DDRB |= (1 << 0);	// B0 is an output
-	DDRB &= ~(1 << 1);	// B1 is an input
-	
-	// Blink that damn LED
-	struct state s = { start, 0, 0, 0 };
-	while (s.next)
-	{
-		s.button = PINB & (1 << 1);
-		s.next(&s);
-		
-		if (s.led_on)
-			PORTB |= 1;
-		else
-			PORTB &= ~1;
-			
-		wait_avr(50);
-	}
+  // Initialize
+  board_init();
+  ini_avr();
 
-	return 0;
+  // Configure GPIO pins
+  DDRB |= (1 << 0);  // B0 is an output
+  DDRB &= ~(1 << 1); // B1 is an input
+
+  // Blink that damn LED
+  struct state s = {start, 0, 0, 0};
+  while (s.next)
+  {
+    s.button = PINB & (1 << 1);
+    s.next(&s);
+
+    if (s.led_on)
+      PORTB |= 1;
+    else
+      PORTB &= ~1;
+
+    wait_avr(50);
+  }
+
+  return 0;
 }
