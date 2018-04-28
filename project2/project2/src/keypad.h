93254eadce8e463aa412443233cbf7b1965fe68e
Sam Balana <sambalana247@gmail.com>
2018-04-28 01:12:10 -0700

    feat(project2): Extend functionality of the keypad
    Using an enum for the key pressed was a great idea given to me by
    our lab instructor, Sam. Genius.

---
+++ project2/project2/src/keypad.h
@@ -0,0 +1,29 @@
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+#include "timer.h"
+
+enum key {
+    KEY_NONE = 0,
+    KEY_1,
+    KEY_2,
+    KEY_3,
+    KEY_A,
+    KEY_4,
+    KEY_5,
+    KEY_6,
+    KEY_B,
+    KEY_7,
+    KEY_8,
+    KEY_9,
+    KEY_C,
+    KEY_STAR,
+    KEY_0,
+    KEY_POUND,
+    KEY_D,
+};
+
+int is_pressed(int row, int col);
+enum key get_key();
+
+#endif
