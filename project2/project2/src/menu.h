e268248d1b45b206f44e77feb6767e4e83ab5eda
David Brady <djbrady@uci.edu>
2018-05-01 16:02:53 -0700

    increment changed to work like FSM. Decrements in. No blinking

--- project2/project2/src/menu.h
+++ project2/project2/src/menu.h
@@ -6,11 +6,14 @@
 #include "clock.h"
 
 void menu_start(struct state *);
-void menu_controller(struct state *);
-void advance_option(struct state *);
-void increment_selected(struct state *);
-void decrement_selected(struct state *);
-void turn_menu_off(struct state *);
+
+void edit_none(struct state *);
+void edit_month(struct state *);
+void edit_day(struct state *);
+void edit_year(struct state *);
+void edit_hour(struct state *);
+void edit_minute(struct state *);
+void edit_second(struct state *);
 
 void increment_month(struct state *);
 void increment_day(struct state *);
@@ -19,7 +22,11 @@ void increment_hour(struct state *);
 void increment_minute(struct state *);
 void increment_second(struct state *);
 
-// TODO (David): Complete the rest of the states
-
+void decrement_month(struct state *);
+void decrement_day(struct state *);
+void decrement_year(struct state *);
+void decrement_hour(struct state *);
+void decrement_minute(struct state *);
+void decrement_second(struct state *);
 
 #endif
