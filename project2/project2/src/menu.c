e05401a37e0f4b206f0ae9392b7bd684c7206ccd
Sam Balana <sambalana247@gmail.com>
2018-05-03 23:32:25 -0700

    fix(menu): Correct syntax for switch statements

--- project2/project2/src/menu.c
+++ project2/project2/src/menu.c
@@ -4,75 +4,110 @@
 
 void menu_start(struct state *s)
 {
-    s->editing = 0;
     s->next_menu = edit_none;
 }
 
-void edit_none(struct state *)
+void edit_none(struct state *s)
 {
     switch (s->key_pressed)
     {
-        case KEY_A : s->next_menu = edit_month;
-        default: break;
+    case KEY_A:
+        s->next_menu = edit_month;
+        break;
     }
 }
 
-void edit_month(struct state *)
+void edit_month(struct state *s)
 {
     switch (s->key_pressed)
     {
-        case KEY_A : s->next_menu = edit_day;
-        case KEY_B : increment_month(s);
-        case KEY_C : decrement_month(s);
-        case KEY_D : s->next_menu = edit_none;
-        default: break;
+    case KEY_A:
+        s->next_menu = edit_day;
+        break;
+    case KEY_B:
+        increment_month(s);
+        break;
+    case KEY_C:
+        decrement_month(s);
+        break;
+    case KEY_D:
+        s->next_menu = edit_none;
+        break;
     }
 }
 
-void edit_day(struct state *)
+void edit_day(struct state *s)
 {
     switch (s->key_pressed)
     {
-        case KEY_A : s->next_menu = edit_year;
-        case KEY_B : increment_day(s);
-        case KEY_C : decrement_day(s);
-        case KEY_D : s->next_menu = edit_none;
-        default: break;
+    case KEY_A:
+        s->next_menu = edit_year;
+        break;
+    case KEY_B:
+        increment_day(s);
+        break;
+    case KEY_C:
+        decrement_day(s);
+        break;
+    case KEY_D:
+        s->next_menu = edit_none;
+        break;
     }
 }
 
-void edit_year(struct state *)
+void edit_year(struct state *s)
 {
     switch (s->key_pressed)
     {
-        case KEY_A : s->next_menu = edit_hour;
-        case KEY_B : increment_year(s);
-        case KEY_C : decrement_year(s);
-        case KEY_D : s->next_menu = edit_none;
-        default: break;
+    case KEY_A:
+        s->next_menu = edit_hour;
+        break;
+    case KEY_B:
+        increment_year(s);
+        break;
+    case KEY_C:
+        decrement_year(s);
+        break;
+    case KEY_D:
+        s->next_menu = edit_none;
+        break;
     }
 }
 
-void edit_hour(struct state *)
+void edit_hour(struct state *s)
 {
     switch (s->key_pressed)
     {
-        case KEY_A : s->next_menu = edit_minute;
-        case KEY_B : increment_hour(s);
-        case KEY_C : decrement_hour(s);
-        case KEY_D : s->next_menu = edit_second;
-        default: break;
+    case KEY_A:
+        s->next_menu = edit_minute;
+        break;
+    case KEY_B:
+        increment_hour(s);
+        break;
+    case KEY_C:
+        decrement_hour(s);
+        break;
+    case KEY_D:
+        s->next_menu = edit_second;
+        break;
     }
 }
 
-void edit_second(struct state *)
+void edit_second(struct state *s)
 {
     switch (s->key_pressed)
     {
-        case KEY_A : s->next_menu = edit_none;
-        case KEY_B: increment_second(s);
-        case KEY_C: decrement_second(s);
-        case KEY_D: s->next_menu = edit_none;
-        default: break;
+    case KEY_A:
+        s->next_menu = edit_none;
+        break;
+    case KEY_B:
+        increment_second(s);
+        break;
+    case KEY_C:
+        decrement_second(s);
+        break;
+    case KEY_D:
+        s->next_menu = edit_none;
+        break;
     }
 }
