d9dee37706bc579272daab8e1f3eb257d4b171a5
Sam Balana <sambalana247@gmail.com>
2018-05-05 11:58:22 -0700

    feat(menu): Finish menu states

--- project2/project2/src/menu.c
+++ project2/project2/src/menu.c
@@ -17,6 +17,25 @@ void edit_none(struct state *s)
     }
 }
 
+void edit_year(struct state *s)
+{
+    switch (s->key_pressed)
+    {
+    case KEY_A:
+        s->next_menu = edit_hour;
+        break;
+    case KEY_B:
+        increment_year(s);
+        break;
+    case KEY_C:
+        decrement_year(s);
+        break;
+    case KEY_D:
+        s->next_menu = edit_none;
+        break;
+    }
+}
+
 void edit_month(struct state *s)
 {
     switch (s->key_pressed)
@@ -41,7 +60,7 @@ void edit_day(struct state *s)
     switch (s->key_pressed)
     {
     case KEY_A:
-        s->next_menu = edit_year;
+        s->next_menu = edit_month;
         break;
     case KEY_B:
         increment_day(s);
@@ -50,26 +69,7 @@ void edit_day(struct state *s)
         decrement_day(s);
         break;
     case KEY_D:
-        s->next_menu = edit_none;
-        break;
-    }
-}
-
-void edit_year(struct state *s)
-{
-    switch (s->key_pressed)
-    {
-    case KEY_A:
         s->next_menu = edit_hour;
-        break;
-    case KEY_B:
-        increment_year(s);
-        break;
-    case KEY_C:
-        decrement_year(s);
-        break;
-    case KEY_D:
-        s->next_menu = edit_none;
         break;
     }
 }
@@ -79,13 +79,32 @@ void edit_hour(struct state *s)
     switch (s->key_pressed)
     {
     case KEY_A:
-        s->next_menu = edit_none;
+        s->next_menu = edit_day;
         break;
     case KEY_B:
         increment_hour(s);
         break;
     case KEY_C:
         decrement_hour(s);
+        break;
+    case KEY_D:
+        s->next_menu = edit_minute;
+        break;
+    }
+}
+
+void edit_minute(struct state *s)
+{
+    switch (s->key_pressed)
+    {
+    case KEY_A:
+        s->next_menu = edit_hour;
+        break;
+    case KEY_B:
+        increment_minute(s);
+        break;
+    case KEY_C:
+        decrement_minute(s);
         break;
     case KEY_D:
         s->next_menu = edit_second;
@@ -98,7 +117,7 @@ void edit_second(struct state *s)
     switch (s->key_pressed)
     {
     case KEY_A:
-        s->next_menu = edit_none;
+        s->next_menu = edit_minute;
         break;
     case KEY_B:
         increment_second(s);
