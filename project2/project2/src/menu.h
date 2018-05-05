767f234ffa67341fa4fb107b1a6e9a4d8458e5e8
Sam Balana <sambalana247@gmail.com>
2018-05-05 15:42:13 -0700

    fix(menu): Finish function declarations

--- project2/project2/src/menu.h
+++ project2/project2/src/menu.h
@@ -18,9 +18,7 @@ void edit_month_pressed(struct state *);
 void edit_day_pressed(struct state *);
 void edit_year_pressed(struct state *);
 void edit_hour_pressed(struct state *);
-void edit_minute(struct state *);
-void edit_second(struct state *);
-
-
+void edit_minute_pressed(struct state *);
+void edit_second_pressed(struct state *);
 
 #endif
