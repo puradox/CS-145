67f66500c4c1b2f1303b82e7f8f34ee64e4c0bc2
David Brady <djbrady@uci.edu>
2018-05-04 22:37:37 -0700

    code changed to use FSM style. decrementing in. no blinking

--- project2/project2/src/menu.c
+++ project2/project2/src/menu.c
@@ -1,113 +1,177 @@
 #include "menu.h"
-#include "keypad.h"
-#include "date_utils.h"
 
 void menu_start(struct state *s)
 {
     s->next_menu = edit_none;
 }
 
-void edit_none(struct state *s)
+void edit_none(struct state *)
 {
     switch (s->key_pressed)
     {
-    case KEY_A:
-        s->next_menu = edit_month;
-        break;
+        case KEY_A : s->next_menu = edit_month;
+        default: break;
     }
 }
 
-void edit_month(struct state *s)
+void edit_month(struct state *)
 {
     switch (s->key_pressed)
     {
-    case KEY_A:
-        s->next_menu = edit_day;
-        break;
-    case KEY_B:
-        increment_month(s);
-        break;
-    case KEY_C:
-        decrement_month(s);
-        break;
-    case KEY_D:
-        s->next_menu = edit_none;
-        break;
+        case KEY_A : s->next_menu = edit_day;
+        case KEY_B : increment_month(s);
+        case KEY_C : decrement_month(s);
+        case KEY_D : s->next_menu = edit_none;
+        default: break;
     }
 }
 
-void edit_day(struct state *s)
+void edit_day(struct state *)
 {
     switch (s->key_pressed)
     {
-    case KEY_A:
-        s->next_menu = edit_year;
-        break;
-    case KEY_B:
-        increment_day(s);
-        break;
-    case KEY_C:
-        decrement_day(s);
-        break;
-    case KEY_D:
-        s->next_menu = edit_none;
-        break;
+        case KEY_A : s->next_menu = edit_year;
+        case KEY_B : increment_day(s);
+        case KEY_C : decrement_day(s);
+        case KEY_D : s->next_menu = edit_none;
+        default: break;
     }
 }
 
-void edit_year(struct state *s)
+void edit_year(struct state *)
 {
     switch (s->key_pressed)
     {
-    case KEY_A:
-        s->next_menu = edit_hour;
-        break;
-    case KEY_B:
+        case KEY_A : s->next_menu = edit_hour;
+        case KEY_B : increment_year(s);
+        case KEY_C : decrement_year(s);
+        case KEY_D : s->next_menu = edit_none;
+        default: break;
+    }
+}
+
+void edit_hour(struct state *)
+{
+    switch (s->key_pressed)
+    {
+        case KEY_A : s->next_menu = edit_minute;
+        case KEY_B : increment_hour(s);
+        case KEY_C : decrement_hour(s);
+        case KEY_D : s->next_menu = edit_second;
+        default: break;
+    }
+}
+
+void edit_second(struct state *)
+{
+    switch (s->key_pressed)
+    {
+        case KEY_A : s->next_menu = edit_none;
+        case KEY_B: increment_second(s);
+        case KEY_C: decrement_second(s);
+        case KEY_D: s->next_menu = edit_none;
+        default: break;
+    }
+}
+
+
+void increment_month(struct state *s)
+{
+    s->month++;
+    if (s->month >= 12) {
         increment_year(s);
-        break;
-    case KEY_C:
-        decrement_year(s);
-        break;
-    case KEY_D:
-        s->next_menu = edit_none;
-        break;
+        s->month -= 11;
     }
 }
 
-void edit_hour(struct state *s)
+void increment_day(struct state *s)
 {
-    switch (s->key_pressed)
-    {
-    case KEY_A:
-        s->next_menu = edit_none;
-        break;
-    case KEY_B:
+    s->day++;
+    int days_month = days_in_month(s->month, s->year);
+    if (s->day >= days_month) {
+        increment_month(s);
+        s->day -= days_month;
+    }
+}
+
+void increment_year(struct state *s)
+{
+    s->year++;
+}
+
+void increment_hour(struct state *s)
+{
+    s->hour++;
+    if (s->hour >= 24) {
+        increment_day(s);
+        s->hour -= 24;
+    }
+}
+
+void increment_minute(struct state *s)
+{
+    s->minute++;
+    if (s->minute >= 60) {
         increment_hour(s);
-        break;
-    case KEY_C:
-        decrement_hour(s);
-        break;
-    case KEY_D:
-        s->next_menu = edit_second;
-        break;
+        s->minute -= 60;
     }
 }
 
-void edit_second(struct state *s)
+void increment_second(struct state *s)
 {
-    switch (s->key_pressed)
-    {
-    case KEY_A:
-        s->next_menu = edit_none;
-        break;
-    case KEY_B:
-        increment_second(s);
-        break;
-    case KEY_C:
-        decrement_second(s);
-        break;
-    case KEY_D:
-        s->next_menu = edit_none;
-        break;
+    s->second++;
+    if (s->minute >= 60) {
+        increment_minute(s);
+        s->second -= 60;
+    }
+}
+
+void decrement_month(struct state *s)
+{
+    s->month--;
+    if (s->month < 1) {
+        decrement_year(s);
+        s->month += 12;
+    }
+}
+
+void decrement_day(struct state *s)
+{
+    s->day--;
+    if (s->day < 1) {
+        decrement_month(s);
+        s->day = days_in_month(s->month, s->year);
+    }
+}
+
+void decrement_year(struct state *)
+{
+    s->year--;
+}
+
+void decrement_hour(struct state *)
+{
+    s->hour--;
+    if (s->hour < 0) { // 0 or 1?
+        decrement_day(s);
+        s->hour += 24;
+    }
+}
+
+void decrement_minute(struct state *s)
+{
+    s->minute--;
+    if (s->minute < 0) {
+        decrement_hour(s);
+        s->minute += 60;
+    }
+}
+
+void decrement_second(struct state *s)
+{
+    s->second--;
+    if (s->minute < 0) {
+        decrement_minute(s);
+        s->second += 60;
     }
 }
--- project2/project2/src/menu.h
+++ project2/project2/src/menu.h
@@ -2,6 +2,8 @@
 #define MENU_H
 
 #include "state.h"
+#include "keypad.h"
+#include "clock.h"
 
 void menu_start(struct state *);
 
@@ -12,5 +14,19 @@ void edit_year(struct state *);
 void edit_hour(struct state *);
 void edit_minute(struct state *);
 void edit_second(struct state *);
+
+void increment_month(struct state *);
+void increment_day(struct state *);
+void increment_year(struct state *);
+void increment_hour(struct state *);
+void increment_minute(struct state *);
+void increment_second(struct state *);
+
+void decrement_month(struct state *);
+void decrement_day(struct state *);
+void decrement_year(struct state *);
+void decrement_hour(struct state *);
+void decrement_minute(struct state *);
+void decrement_second(struct state *);
 
 #endif
