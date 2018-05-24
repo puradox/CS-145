945cc6e66da14b3fe1a4cd6a1ed2d66847409ced
David Brady <david.j.brady@att.net>
2018-05-24 11:23:57 -0700

    initial proj4

---
+++ project4/project4/main.c
@@ -0,0 +1,18 @@
+/*
+ * project4.c
+ *
+ * Created: 5/24/2018 11:20:16 AM
+ * Author : David
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
