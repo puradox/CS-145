4bb60922f792661b5efaeeb85b64ed7f250414e6
Sam Balana <sambalana247@gmail.com>
2018-06-06 23:21:28 -0700

    chore(project5): Initialize project

---
+++ project5/project5/main.c
@@ -0,0 +1,18 @@
+/*
+ * project5.c
+ *
+ * Created: 6/6/2018 11:20:45 PM
+ * Author : Sam
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
